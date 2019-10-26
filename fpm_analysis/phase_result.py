dcbb989b65ce46fceb14fe98168f1c0d38903da2
Alejandro <ale.exactas@gmail.com>
2019-10-25 21:54:44 -0300

    Un dos tres cuatro cinco seis

--- fpm_analysis/phase_result.py
+++ fpm_analysis/phase_result.py
@@ -1,6 +1,8 @@
 import logging
 import pathlib
-from typing import Dict
+from functools import wraps
+from typing import Dict, Tuple
+import inspect
 
 import click
 import h5py
@@ -14,6 +16,36 @@ logger = logging.getLogger(__name__)
 COLORS = {'r': 0.630, 'g': 0.520, 'b': 0.480}
 
 
+def optional_axes(rows=1, columns=1):
+    def decorator(func):
+        @wraps(func)
+        def wrapped(self, ax=None, force_show=False, subplot_kw=None, fig_kw=None, *args, **kwargs):
+            if ax is None:
+                if fig_kw is None:
+                    fig_kw = {}
+                fig, ax = plt.subplots(rows, columns, subplot_kw=subplot_kw, **fig_kw)
+                force_show = True
+            r = func(self, ax=ax, *args, **kwargs)
+            if force_show is True:
+                plt.show()
+            return r
+        return wrapped
+    return decorator
+
+
+def apply_roi(data, roi: Tuple[Tuple[int, int], Tuple[int, int]]):
+    if roi is not None:
+        if isinstance(data, np.ndarray):
+            data = data[roi[1][0]:roi[1][1], roi[0][0]:roi[0][1]]
+        elif isinstance(data, (tuple, list)):
+            data = [d[roi[1][0]:roi[1][1], roi[0][0]:roi[0][1]] for d in data]
+        elif isinstance(data, dict):
+            data = {c: d[roi[1][0]:roi[1][1], roi[0][0]:roi[0][1]] for c, d in data.items()}
+        else:
+            raise TypeError
+    return data
+
+
 class FPMResult:
     def __init__(self, *args, **kwargs):
         self._sample: Dict[str, np.ndarray] = {}
@@ -21,6 +53,7 @@ class FPMResult:
         self._phase: Dict[str, np.ndarray] = {}
         self._unwrapped: Dict[str, np.ndarray] = {}
         self._colors = None
+        self._unwrap = True
 
     def load_hdf5(self, data):
         data = pathlib.Path(data)
@@ -41,124 +74,35 @@ class FPMResult:
             # self._unwrapped[color] -= self._unwrapped[color].min()
             logger.info('Sample added')
 
-    def plot(self, color='r', unwrap=True, ax=None):
-        if ax is None:
-            fig, ax = plt.subplots(1, 2, sharex='all', sharey='all')
+    def get_quotient(self, quotient: str = 'r/b', unwrap=True, roi=None):
         phase = self._unwrapped if unwrap is True else self._phase
-        ax[0].imshow(self._amplitude[color])
-        ax[1].imshow(phase[color])
-        if ax is None:
-            plt.show()
-
-    def plot_amplitude(self, color='r', ax=None):
-        if ax is None:
-            fig, ax = plt.subplots(1, 1)
-        ax.imshow(self._amplitude[color])
-        if ax is None:
-            plt.show()
-
-    def plot_phase(self, color='r', unwrap=True, ax=None):
-        if ax is None:
-            fig, ax = plt.subplots(1, 1)
-        phase = self._unwrapped if unwrap is True else self._phase
-        ax.imshow(phase[color])
-        if ax is None:
-            plt.show()
-
-        # plt.hist(phase[color].flatten())
-        # plt.show()
-
-    def plot_diff(self, subtract: str = 'r-b', unwrap=True, ax=None):
-        if ax is None:
-            fig, ax = plt.subplots(1, 1)
-        phase = self._unwrapped if unwrap is True else self._phase
-        c_a = subtract.split('-')[0]
-        c_b = subtract.split('-')[1]
-        ax.imshow(np.abs(phase[c_a] - phase[c_b]))
-        if ax is None:
-            plt.show()
-
-    def get_quotient(self, quotient: str = 'r/b', unwrap=True):
-        phase = self._unwrapped if unwrap is True else self._phase
+        phase = apply_roi(phase, roi)
         c_a = quotient.split('/')[0]
         c_b = quotient.split('/')[1]
-        w = wx = COLORS[c_a] / COLORS[c_b]
+        w = COLORS[c_a] / COLORS[c_b]
         return phase[c_a] / phase[c_b] * w
 
-    def get_quotient_2d(self, x: str = 'r/g', y: str = 'g/b', unwrap=True):
-        xc = self.get_quotient(x, unwrap)
-        yc = self.get_quotient(y, unwrap)
-        return xc, yc
+    @optional_axes()
+    def plot_amplitude(self, ax, color, roi=None):
+        ax.imshow(apply_roi(self._amplitude[color], roi))
 
-    def plot_quotient(self, quotient: str = 'r/b', unwrap=True, ax=None):
-        if ax is None:
-            fig, ax = plt.subplots(1, 1)
-        q = self.get_quotient(quotient, unwrap)
-        r = ax.imshow(q)
-        if ax is None:
-            plt.show()
-        else:
-            return r
+    @optional_axes()
+    def plot_phase(self, ax, color, roi=None):
+        ax.imshow(apply_roi(self._phase[color], roi))
 
-    def plot_quotient_2d(self, x: str = 'r/g', y: str = 'g/b', unwrap=True, threshold=None,
-                         roi=None, ax=None):
-        if roi is None:
-            if ax is None:
-                fig, ax = plt.subplots(1, 1)
-            else:
-                if not isinstance(ax, plt.Axes):
-                    raise TypeError
-        else:
-            if ax is None:
-                fig, ax = plt.subplots(1, 2, sharex='all', sharey='all')
-            else:
-                if not all([isinstance(x, plt.Axes) for x in ax]):
-                    raise TypeError
+    @optional_axes()
+    def plot_unwrapped(self, ax, color, roi=None):
+        ax.imshow(apply_roi(self._unwrapped[color], roi))
 
-        xc, yc = self.get_quotient_2d(x, y, unwrap)
-        if threshold is None and roi is None:
-            plt.plot(xc.flatten(), yc.flatten(), 'ro', markersize=0.1)
-        else:
-            if threshold is not None and roi is None:
-                mask = self._unwrapped['r'].flatten() > threshold
-                _mask = self._unwrapped['r'].flatten() <= threshold
+    @optional_axes()
+    def plot_quotient(self, ax, q='r/g', roi=None, unwrap=True):
+        ax.imshow(self.get_quotient(quotient=q, unwrap=unwrap, roi=roi))
 
-                ax[0].imshow(self._unwrapped['r'])
-                ax[1].imshow(self._unwrapped['r'] > threshold)
-                plt.tight_layout()
-                fig_manager = plt.get_current_fig_manager()
-                fig_manager.window.showMaximized()
-
-            elif roi is not None and threshold is None:
-                mask = np.zeros_like(self._unwrapped['r'], dtype=bool)
-                mask[:] = False
-                mask[roi[1][0]:roi[1][1], roi[0][0]:roi[0][1]] = True
-
-                _mask = np.ones_like(self._unwrapped['r'], dtype=bool)
-                _mask[:] = True
-                _mask[roi[1][0]:roi[1][1], roi[0][0]:roi[0][1]] = False
-
-                fig, ax = plt.subplots(1, 2, sharex='all', sharey='all')
-                ax[0].imshow(self._unwrapped['r'])
-                ax[1].imshow(mask)
-                plt.tight_layout()
-                fig_manager = plt.get_current_fig_manager()
-                fig_manager.window.showMaximized()
-
-                mask = mask.flatten()
-                _mask = _mask.flatten()
-            else:
-                raise ValueError
-            fig, ax = plt.subplots(1, 2, sharex='all', sharey='all')
-            ax[0].plot(xc.flatten()[mask], yc.flatten()[mask], 'ro', markersize=0.5, alpha=0.5)
-            ax[1].plot(xc.flatten()[_mask], yc.flatten()[_mask], 'bo', markersize=0.5, alpha=0.5)
-            ax[0].set_xlim(-30, 30)
-            ax[0].set_ylim(-10, 10)
-            plt.tight_layout()
-            fig_manager = plt.get_current_fig_manager()
-            fig_manager.window.showMaximized()
-        if ax is None:
-            plt.show()
+    @optional_axes()
+    def plot_quotient_2d(self, ax, x: str = 'r/g', y: str = 'g/b', roi=None, unwrap=True):
+        xc = self.get_quotient(x, roi=roi, unwrap=unwrap)
+        yc = self.get_quotient(y, roi=roi, unwrap=unwrap)
+        ax.plot(xc.flatten(), yc.flatten(), 'ro', markersize=0.1)
 
 
 @click.command()
@@ -167,17 +111,10 @@ class FPMResult:
 def plot_quotient(data, unwrap):
     result = FPMResult()
     result.load_hdf5(data)
-    # result.plot(color='r', unwrap=unwrap)
-    # result.plot(color='g', unwrap=unwrap)
-    # result.plot(color='b', unwrap=unwrap)
-    # result.plot_phase('r', unwrap=unwrap)
-    # result.plot_phase('g', unwrap=unwrap)
-    # result.plot_phase('b', unwrap=unwrap)
-    # result.plot_quotient('r/g', unwrap=unwrap)
-    # result.plot_quotient('b/g', unwrap=unwrap)
-    # result.plot_quotient('r/b', unwrap=unwrap)
-    result.plot_quotient_2d(x='r/g', y='b/g', unwrap=unwrap, threshold=0.0)
-    result.plot_quotient_2d(x='r/g', y='b/g', unwrap=unwrap, roi=((0, 200), (1000, 1200)))
+    result.plot_amplitude(color='r')
+    result.plot_phase(color='r')
+    result.plot_unwrapped(color='r')
+    result.plot_quotient_2d(x='r/g', y='b/g', unwrap=unwrap)
 
 
 if __name__ == '__main__':
